875da554e28304819ca54d0b7822592a317a79a1
quan-IT <nguyenhuuquan01122k5@gmail.com>
2026-06-10 20:24:13 +0700

    up routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,18 +7,30 @@ import ProductDetailPage from "@/pages/ProductDetailPage";
 import ShirtPage from "@/pages/ShirtPage";
 import CartPage from "@/pages/CartPage";
 
+// Quân thêm
+import OrdersPage from "@/pages/OrdersPage";
 
 // Những trang có thể vào mà không cần đăng nhập
 const publicRoutes = [
-    { path: '/', component: HomePage, label: 'Trang chủ' }, 
-    { path: '/shirt', component: ShirtPage, label: 'Áo' },
-    { path: '/pants', component: PantsPage, label: 'Quần' },
-    { path: '/accessories', component: AccessoriesPage, label: 'Phụ kiện' },
-    { path: '/new-collection', component: NewCollectionPage, label: 'Bộ sưu tập mới' },
-    { path: '/product/:id', component: ProductDetailPage, label: 'Chi tiết sản phẩm' }
+  { path: "/", component: HomePage, label: "Trang chủ" },
+  { path: "/shirt", component: ShirtPage, label: "Áo" },
+  { path: "/pants", component: PantsPage, label: "Quần" },
+  { path: "/accessories", component: AccessoriesPage, label: "Phụ kiện" },
+  {
+    path: "/new-collection",
+    component: NewCollectionPage,
+    label: "Bộ sưu tập mới",
+  },
+  {
+    path: "/product/:id",
+    component: ProductDetailPage,
+    label: "Chi tiết sản phẩm",
+  },
 ];
 
 // Những trang cần đăng nhập mới vào được;
-const privateRoutes = [];
+const privateRoutes = [
+  { path: "/orders", component: OrdersPage, label: "Đơn hàng" },
+];
 
 export { publicRoutes, privateRoutes };
